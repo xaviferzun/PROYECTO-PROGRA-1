428c26ef6dc654f3dc7a37df2ae1ed6e6ba80342
Xavier Fernández <xaviferzun@gmail.com>
2023-11-20 19:18:26 -0600

    Pruebita

---
+++ PRY-Xavier-Alonso/src/App/Aplicacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package App;
+
+/**
+ *
+ * @author XAVIER FERNÁNDEZ Z
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
